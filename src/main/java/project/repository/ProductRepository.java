d726e79f0662d76cc4872320e78785384438057d
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-11-06 12:39:02 +0200

    Registration - added giving award for new user

--- src/main/java/project/repository/ProductRepository.java
+++ src/main/java/project/repository/ProductRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import project.entity.Product;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor<Product> {
     @Query(value = "SELECT p FROM Product p LEFT JOIN FETCH p.additiveTypes a WHERE a.deleted=false AND a.status=true AND p.id= :id")
     Product findProductWithAdditiveTypesById(@Param("id")Long id);
+    @Query(value = "SELECT p FROM Product p WHERE p.deleted=false AND p.status=true AND p.price < 80")
+    List<Product> findProductsForAward();
 }
--- src/main/java/project/serviceImpl/AuthenticationServiceImpl.java
+++ src/main/java/project/serviceImpl/AuthenticationServiceImpl.java
@@ -5,28 +5,36 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import project.entity.Product;
 import project.entity.Role;
 import project.entity.User;
+import project.entity.UserStatus;
 import project.model.authenticationModel.AuthenticationRequest;
 import project.model.authenticationModel.AuthenticationResponse;
 import project.model.userModel.UserRequest;
+import project.repository.ProductRepository;
 import project.repository.UserRepository;
 import project.service.AuthenticationService;
 import project.service.JwtService;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final ProductRepository productRepository;
 
-    public AuthenticationServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
+    public AuthenticationServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, JwtService jwtService, AuthenticationManager authenticationManager, ProductRepository productRepository) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.jwtService = jwtService;
         this.authenticationManager = authenticationManager;
+        this.productRepository = productRepository;
     }
 
     @Override
@@ -37,7 +45,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         user.setEmail(userRequest.getEmail());
         user.setPassword(bCryptPasswordEncoder.encode(userRequest.getPassword()));
         user.setRole(Role.USER);
+        user.setStatus(UserStatus.NEW);
         user.setRegistrationDate(LocalDate.now());
+        List<Product> products = productRepository.findProductsForAward();
+        List<Product> userProducts = new ArrayList<>(1);
+        userProducts.add(products.get(0));
+        user.setProducts(userProducts);
         userRepository.save(user);
         AuthenticationResponse authenticationResponse = new AuthenticationResponse();
         authenticationResponse.setToken(jwtService.generateToken(user));
