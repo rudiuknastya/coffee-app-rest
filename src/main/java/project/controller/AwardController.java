e7b36595bdf08012034f2a77197ae133729238c1
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-11-13 15:44:32 +0200

    Award - changed pageable

--- src/main/java/project/controller/AwardController.java
+++ src/main/java/project/controller/AwardController.java
@@ -35,30 +35,15 @@ public class AwardController {
             @ApiResponse(responseCode = "400", description = "Bad request")})
     @GetMapping("/awards")
     Page<AwardDTO> getUserAwards(PageableDTO pageableDTO){
-        int page = 0;
-        int size = 5;
-        if(pageableDTO.getPage() >= 0){
-            page = pageableDTO.getPage();
-        }
-        if(pageableDTO.getSize() > 0){
-            size = pageableDTO.getSize();
-        }
         Pageable pageable;
-        if(pageableDTO.getSortField() != null){
-            Sort sort = Sort.by("id").ascending();
-            if(pageableDTO.getSortDirection() != null){
-                if(pageableDTO.getSortDirection().equals("ASC")){
-                    sort = Sort.by(pageableDTO.getSortField()).ascending();
-                }
-                if(pageableDTO.getSortDirection().equals("DESC")){
-                    sort = Sort.by(pageableDTO.getSortField()).descending();
-                }
-
-            }
-            pageable = PageRequest.of(page,size,sort);
-        } else {
-            pageable = PageRequest.of(page,size,Sort.by("id").ascending());
+        Sort sort;
+        if(pageableDTO.getSortDirection().equals("DESC")){
+            sort = Sort.by(pageableDTO.getSortField()).descending();
         }
+        else{
+            sort = Sort.by(pageableDTO.getSortField()).ascending();
+        }
+        pageable = PageRequest.of(pageableDTO.getPage(), pageableDTO.getSize(),sort);
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication()
                 .getPrincipal();
         String email = userDetails.getUsername();
