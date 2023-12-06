f80ebb3130a8985234b11db23d2ccdaf2667c6bf
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-12-06 13:29:42 +0200

    Added configuration for swagger

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -1,10 +1,12 @@
 package project;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
-
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 @EnableAsync
 public class App extends SpringBootServletInitializer {
