7c8f5b21b52255298c9da75cb1ef6faa9943eb02
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-12-20 12:05:10 +0200

    Added description for the whole project

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -1,12 +1,16 @@
 package project;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
-@OpenAPIDefinition(servers = {@Server(url = "/Coffee_App_A_Rudiuk_Rest/", description = "Default Server URL")})
+@OpenAPIDefinition(
+        info = @Info(title = "Coffee-app Rest Api",description = "Coffee-app Rest Api documentation",version = "v1"),
+        servers = {@Server(url = "/Coffee_App_A_Rudiuk_Rest/", description = "Default Server URL")}
+)
 @SpringBootApplication
 @EnableAsync
 public class App extends SpringBootServletInitializer {
